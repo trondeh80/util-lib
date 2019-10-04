573bece61a291964a192c7a63f926ac8e3fe044d
trondeh80 <trond.erling@gmail.com>
2019-10-05 01:26:09 +0200

    Add transpiled version in dist

--- dist/arrays.js
+++ dist/arrays.js
@@ -4,15 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.arrayFind = arrayFind;
-exports.joinArrays = joinArrays;
-
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
-
-function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
 function arrayFind(list, predicateFn) {
   for (var i = 0; i < list.length; i++) {
@@ -22,12 +13,4 @@ function arrayFind(list, predicateFn) {
   }
 
   return void 0;
-}
-
-function joinArrays(array1) {
-  for (var _len = arguments.length, rest = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    rest[_key - 1] = arguments[_key];
-  }
-
-  return [].concat(_toConsumableArray(array1), rest);
 }
